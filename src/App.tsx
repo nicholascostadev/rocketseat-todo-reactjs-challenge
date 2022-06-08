4a0f5622d526aa9715002af97c51ae3521cceec3
nicholascostadev <nicholascostadev@gmail.com>
2022-06-08 16:47:43 -0300

    Fix minor issue of not showing text when user doens't have tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,84 @@
-import './global.css';
-import styles from './App.module.css';
+import "./global.css";
+import styles from "./App.module.css";
 
-import { Header } from './Header';
-import { Input } from './Input';
-import { TasksHeader } from './TasksHeader';
-import { useEffect, useState } from 'react';
-import { Todo } from './Todo';
-import { ToastContainer } from 'react-toastify';
+import { Header } from "./Header";
+import { Input } from "./Input";
+import { TasksHeader } from "./TasksHeader";
+import { useEffect, useState } from "react";
+import { Todo } from "./Todo";
+import clipboardImage from "./assets/Clipboard.svg";
+import { ToastContainer } from "react-toastify";
 
 interface TodoType {
-	id: string;
-	isDone: boolean;
-	content: string;
+  id: string;
+  isDone: boolean;
+  content: string;
 }
 
 function App() {
-	const [todos, setTodos] = useState<TodoType[]>([]);
+  const [todos, setTodos] = useState<TodoType[]>([]);
 
-	useEffect(() => {
-		const localTodos = localStorage.getItem('todos');
-		if (localTodos) {
-			return setTodos(JSON.parse(localTodos));
-		}
-	}, []);
+  useEffect(() => {
+    const localTodos = localStorage.getItem("todos");
+    if (localTodos) {
+      return setTodos(JSON.parse(localTodos));
+    }
+  }, []);
 
-	const handleAddTodo = (todos: TodoType[]) => {
-		setTodos(todos);
-		localStorage.setItem('todos', JSON.stringify(todos));
-	};
+  const handleAddTodo = (todos: TodoType[]) => {
+    setTodos(todos);
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
 
-	const handleCompleteTodo = (id: string) => {
-		const todoIndexToUpdate = todos.findIndex(todo => todo.id === id);
+  const handleCompleteTodo = (id: string) => {
+    const todoIndexToUpdate = todos.findIndex((todo) => todo.id === id);
 
-		const newTodosArray = [...todos];
+    const newTodosArray = [...todos];
 
-		newTodosArray[todoIndexToUpdate].isDone =
-			!newTodosArray[todoIndexToUpdate].isDone;
+    newTodosArray[todoIndexToUpdate].isDone =
+      !newTodosArray[todoIndexToUpdate].isDone;
 
-		setTodos(newTodosArray);
-		localStorage.setItem('todos', JSON.stringify(newTodosArray));
-	};
+    setTodos(newTodosArray);
+    localStorage.setItem("todos", JSON.stringify(newTodosArray));
+  };
 
-	const handleDeleteTodo = (id: string) => {
-		const newTodosArray = todos.filter(todo => todo.id !== id);
+  const handleDeleteTodo = (id: string) => {
+    const newTodosArray = todos.filter((todo) => todo.id !== id);
 
-		setTodos(newTodosArray);
-		localStorage.setItem('todos', JSON.stringify(newTodosArray));
-	};
+    setTodos(newTodosArray);
+    localStorage.setItem("todos", JSON.stringify(newTodosArray));
+  };
+  console.log(todos);
 
-	return (
-		<div className="App">
-			<Header />
-			<main className={styles.tasksContainer}>
-				<Input onAddTodo={handleAddTodo} todos={todos} />
-				<TasksHeader todosList={todos} />
-				<div className={styles.tasks}>
-					{todos.map(todo => (
-						<Todo
-							key={todo.id}
-							id={todo.id}
-							content={todo.content}
-							isDone={todo.isDone}
-							onTodoDone={handleCompleteTodo}
-							onTodoDelete={handleDeleteTodo}
-						/>
-					))}
-				</div>
-			</main>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Header />
+      <main className={styles.tasksContainer}>
+        <Input onAddTodo={handleAddTodo} todos={todos} />
+        <TasksHeader todosList={todos} />
+        <div className={styles.tasks}>
+          {todos &&
+            todos.map((todo) => (
+              <Todo
+                key={todo.id}
+                id={todo.id}
+                content={todo.content}
+                isDone={todo.isDone}
+                onTodoDone={handleCompleteTodo}
+                onTodoDelete={handleDeleteTodo}
+              />
+            ))}
+          {todos.length === 0 ? (
+            <div className={styles.noTodos}>
+              <img src={clipboardImage} alt="Clipboard image" />
+              <strong>Você ainda não tem tarefas cadastradas</strong>
+              <p>Crie tarefas e organize seus itens a fazer</p>
+            </div>
+          ) : null}
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
